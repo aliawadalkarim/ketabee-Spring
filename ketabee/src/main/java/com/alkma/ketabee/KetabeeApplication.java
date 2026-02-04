4a5118e1d57840fb7d05fba89ad1ddbc2c021dfb
Ali AwadAlakrim <ali.m.awadalkarim@gmail.com>
2026-02-04 12:43:07 +0400

    feat: Initialize Spring Boot project with Gradle, JPA, Security, Web, and Docker Compose support.

---
+++ ketabee/src/main/java/com/alkma/ketabee/KetabeeApplication.java
@@ -0,0 +1,13 @@
+package com.alkma.ketabee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetabeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KetabeeApplication.class, args);
+	}
+
+}
---
+++ ketabee/src/test/java/com/alkma/ketabee/KetabeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkma.ketabee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetabeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
