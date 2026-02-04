f890b7efae45e53bf820991a073228fba4e05f1c
Ali AwadAlakrim <ali.m.awadalkarim@gmail.com>
2026-02-04 12:30:04 +0400

    redoing the application from the start

--- src/main/java/mxd/ketabee/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package mxd.ketabee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mxd.ketabee.model.BookModel;
-
-public interface BookRepository extends JpaRepository<BookModel, Long> {
-
-}
--- src/main/java/mxd/ketabee/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package mxd.ketabee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mxd.ketabee.model.UserModel;
-
-public interface UserRepository extends JpaRepository<UserModel, Long> {
-    UserModel findByUsername(String username);
-}
